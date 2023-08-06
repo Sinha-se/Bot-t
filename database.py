2f1976bd024701295b65dcffb49f21120298d8fd
Sinha-se <141470235+Sinha-se@users.noreply.github.com>
2023-08-06 10:13:04 +0530

    Create database.py

---
+++ database.py
@@ -0,0 +1,24 @@
+# database.py
+presaved_messages = {
+    "hello": {
+        "text": "Hello there!",
+        "image_url": None,
+        "link_url": None
+    },
+    "howareyou": {
+        "text": "I'm doing well, thank you!",
+        "image_url": None,
+        "link_url": None
+    },
+    "bye": {
+        "text": "Goodbye! Have a nice day.",
+        "image_url": None,
+        "link_url": None
+    },
+    "python": {
+        "text": "Python is a great programming language.",
+        "image_url": "https://example.com/python_image.jpg",
+        "link_url": "https://www.python.org/"
+    },
+    # Add more presaved messages here as needed
+}
